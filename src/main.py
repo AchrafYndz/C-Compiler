59bccd9dc51418335166f0e16877113a19b9286c
AchrafYndz <achraf.yandouzi@gmail.com>
2023-05-09 15:27:18 +0200

    Define main function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello, World!")
+import sys
+from antlr4 import *
+
+from src.antlr.CLexer import CLexer
+from src.antlr.CParser import CParser
+
+from src.ASTCreator import ASTCreator
+from src.AST import AST
+from src.ASTErrorListener import ASTErrorListener
 
 
-if __name__ == "__main__":
-    main()
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = CLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = CParser(stream)
+    tree = parser.program()
+
+    parser.removeErrorListeners()
+    parser.addErrorListener(ASTErrorListener())
+
+    walker = ParseTreeWalker()
+    ast_creator = ASTCreator()
+    walker.walk(ast_creator, tree)
+
+    # create AST tree
+    ast_creator.enterProgram(tree)
+    root = ast_creator.root
+
+    root.visualize(filename="test")
+
+    ast = AST(root)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
