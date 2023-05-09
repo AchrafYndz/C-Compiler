c5a84d2ff17e1503119d62bdb8c4b210a96a2b28
AchrafYndz <achraf.yandouzi@gmail.com>
2023-05-09 20:38:38 +0200

    Add SemanticAnalysis tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,47 +6,61 @@ from src.ASTCreator import ASTCreator
 from src.ASTErrorListener import ASTErrorListener
 from src.antlr.CLexer import CLexer
 from src.antlr.CParser import CParser
-
-
-def test_grammar():
-    for directory in ['tests/input/llvm', 'tests/input/semantic_errors']:
-        counter = 0
-        for filename in os.listdir(directory):
-            if filename.endswith('.c'):
-                filepath = os.path.join(directory, filename)
-                print(f"Now handling {filepath}, which is test {counter}")
-                counter += 1
-                input_stream = FileStream(filepath)
-
-                lexer = CLexer(input_stream)
-                stream = CommonTokenStream(lexer)
-                parser = CParser(stream)
-                parser.removeErrorListeners()
-                parser.addErrorListener(ASTErrorListener())
-                tree = parser.program()
+from src.visitors.SemanticAnalysisVisitor import SemanticAnalysisVisitor
 
 
 def test_ast_creation():
-    for directory in ['tests/input/llvm', 'tests/input/semantic_errors']:
-        counter = 0
-        for filename in os.listdir(directory):
-            if filename.endswith('.c'):
-                filepath = os.path.join(directory, filename)
-                print(f"Now handling {filepath}, which is test {counter}")
-                counter += 1
-                input_stream = FileStream(filepath)
-                lexer = CLexer(input_stream)
-                stream = CommonTokenStream(lexer)
-                parser = CParser(stream)
-                tree = parser.program()
+    directory = 'tests/input/llvm'
+    counter = 0
+    for filename in os.listdir(directory):
+        if filename.endswith('.c'):
+            filepath = os.path.join(directory, filename)
+            print(f"Now handling {filepath}, which is test {counter}")
+            counter += 1
+            input_stream = FileStream(filepath)
+            lexer = CLexer(input_stream)
+            stream = CommonTokenStream(lexer)
+            parser = CParser(stream)
+            tree = parser.program()
 
-                parser.removeErrorListeners()
-                parser.addErrorListener(ASTErrorListener())
+            parser.removeErrorListeners()
+            parser.addErrorListener(ASTErrorListener())
 
-                walker = ParseTreeWalker()
-                ast_creator = ASTCreator()
-                walker.walk(ast_creator, tree)
+            walker = ParseTreeWalker()
+            ast_creator = ASTCreator()
+            walker.walk(ast_creator, tree)
 
-                # create AST tree
-                ast_creator.enterProgram(tree)
-                root = ast_creator.root
+            # create AST tree
+            ast_creator.enterProgram(tree)
+            root = ast_creator.root
+
+
+def test_semantic_analysis():
+    directory = 'tests/input/semantic_errors'
+    counter = 0
+    for filename in os.listdir(directory):
+        if filename.endswith('.c'):
+            filepath = os.path.join(directory, filename)
+            print(f"Now handling {filepath}, which is test {counter}")
+            counter += 1
+            input_stream = FileStream(filepath)
+            lexer = CLexer(input_stream)
+            stream = CommonTokenStream(lexer)
+            parser = CParser(stream)
+            tree = parser.program()
+
+            parser.removeErrorListeners()
+            parser.addErrorListener(ASTErrorListener())
+
+            walker = ParseTreeWalker()
+            ast_creator = ASTCreator()
+            walker.walk(ast_creator, tree)
+
+            # create AST tree
+            ast_creator.enterProgram(tree)
+            root = ast_creator.root
+            with pytest.raises(Exception) as exception:
+                ast_semantic_visitor = SemanticAnalysisVisitor()
+                # run semantic analysis
+                ast_semantic_visitor.visitChildren(root)
+            print(str(exception.value))
