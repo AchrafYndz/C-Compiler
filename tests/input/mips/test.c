9ede056ac828021a677da189f9899576367860d4
AchrafYndz <achraf.yandouzi@gmail.com>
2023-05-23 15:55:38 +0200

    Update `test.c`

--- tests/input/mips/test.c
+++ tests/input/mips/test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    char b = 'b';
-    b = 'd';
-    int c =  (b + (b * 3)) / b;
-    printf("%i", c);
-    int d =  (b * a) - (a + 5) + 3;
-    printf("%i", d);
+    int a = 3;
+    int * b = &a;
+    int ** c = &b;
+    int * d = *c;
+    int e = *d;
+
+    printf("%i", e);
     return 0;
 }
