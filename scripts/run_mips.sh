1225e2517b6eb09233ccad75a93a3c9938919899
AchrafYndz <achraf.yandouzi@gmail.com>
2023-05-23 14:35:52 +0200

    Fix MIPS script

--- scripts/run_mips.sh
+++ scripts/run_mips.sh
@@ -1,2 +1,1 @@
-java -jar ../bin/Mars4_5_1_1Unofficial.jar a ../tests/output/mips/test.asm
-#java -jar ../bin/Mars4_5_1_1Unofficial.jar x ../tests/output/mips/test.ams.s
+java -jar ../bin/Mars4_5_1_1Unofficial.jar ../tests/output/mips/test.asm
